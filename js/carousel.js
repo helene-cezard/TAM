25c874b65b4488a6bb49d8e61373acecb4604978
helene-cezard <helene.cezard@gmail.com>
2026-02-24 15:54:47 +0100

    hero page

--- js/carousel.js
+++ js/carousel.js
@@ -1,18 +1,28 @@
-const buttons = document.querySelectorAll('.carousel__btn');
+// const buttons = document.querySelectorAll('.carousel__btn');
 const slides = document.querySelectorAll('.carousel__slide');
 
-buttons.forEach((button) => {
-  button.addEventListener('click', (e) => {
-    const calcNextSlide = e.target.id === 'next' ? 1 : -1;
-    const slideActive = document.querySelector('.active');
+// buttons.forEach((button) => {
+//   button.addEventListener('click', (e) => {
+//     const calcNextSlide = e.target.id === 'next' ? 1 : -1;
+//     const slideActive = document.querySelector('.active');
 
-    newIndex = calcNextSlide + [...slides].indexOf(slideActive);
+//     newIndex = calcNextSlide + [...slides].indexOf(slideActive);
     
-    if (newIndex < 0) newIndex = [...slides].length - 1;
-    if (newIndex >= [...slides].length) newIndex = 0;
-    slides[newIndex].classList.add('active');
+//     if (newIndex < 0) newIndex = [...slides].length - 1;
+//     if (newIndex >= [...slides].length) newIndex = 0;
+//     slides[newIndex].classList.add('active');
 
-    slideActive.classList.remove('active');
+//     slideActive.classList.remove('active');
 
-  });
-});
+//   });
+// });
+
+setInterval(() => {
+  const slideActive = document.querySelector('.active');
+  let newIndex = [...slides].indexOf(slideActive) + 1;
+
+  if (newIndex >= [...slides].length) newIndex = 0;
+  slides[newIndex].classList.add('active');
+
+  slideActive.classList.remove('active');
+}, 5000);
